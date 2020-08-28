277a141c522d79521eb231bcf80d53897f096645
Trung Vo <trung.vo@seldatinc.com>
2020-08-28 10:20:09 +0700

    Initial Commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Trung Vo on 8/28/20.
+//  Copyright © 2020 Trung Vo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
